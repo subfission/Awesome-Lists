359d9bb8ffcc32fdde99bf2eeb0b223b9c92efcf
subfission <subfission@users.no-reply.github.com>
2019-03-24 02:05:37 -0700

    updated modules to master

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git submodule foreach 'git checkout master; git pull'
 git submodule update --init --recursive
+git add .
 git commit -m "updated modules to master"
+git push
 echo "[+] Updated modules"
 
